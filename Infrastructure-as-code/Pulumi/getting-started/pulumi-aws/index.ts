5da69137432f636dfbe291d7d41682b2f85259c9
Miguel Zapata <miguel3lp@hotmail.com>
2024-10-02 17:27:26 -0300

    feat: Allow bucket to host static file
    - Update the program: Update the Bucket declaration to add a website property and make index.html the home page of the website
    - Deploy the website to AWS

--- Infrastructure-as-code/Pulumi/getting-started/pulumi-aws/index.ts
+++ Infrastructure-as-code/Pulumi/getting-started/pulumi-aws/index.ts
@@ -3,18 +3,35 @@ import * as aws from "@pulumi/aws";
 import * as awsx from "@pulumi/awsx";
 
 // Create an AWS resource (S3 Bucket)
-const bucket = new aws.s3.Bucket("my-bucket",{
-    bucket : "my-bucket-without-suffix",
-    website : {
-        indexDocument : "index.html"
+const bucket = new aws.s3.Bucket("my-bucket", {
+    bucket: "my-bucket-without-suffix",
+    website: {
+        indexDocument: "index.html"
     }
 });
 
+const ownershipControls = new aws.s3.BucketOwnershipControls("ownership-controls", {
+    bucket: bucket.id,
+    rule: {
+        objectOwnership: "ObjectWriter"
+    }
+});
+
+const publicAccessBlock = new aws.s3.BucketPublicAccessBlock("public-access-block", {
+    bucket: bucket.id,
+    blockPublicAcls: false,
+});
 
 const bucketObject = new aws.s3.BucketObject("index.html", {
     bucket: bucket.id,
-    source: new pulumi.asset.FileAsset("./public/index.html")
+    source: new pulumi.asset.FileAsset("./public/index.html"),
+    contentType: "text/html",
+    acl: "public-read"
+}, {
+    dependsOn: [ownershipControls, publicAccessBlock]
 });
 
 // Export the name of the bucket
 export const bucketName = bucket.id;
+
+export const bucketEndpoint = pulumi.interpolate`http://${bucket.websiteEndpoint}`;
